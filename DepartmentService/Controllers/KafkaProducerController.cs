09388dd12e93e1285ac6adc21bb72f9dbb2fc61a
RahulSinglaDA <rahul.singla@lngconsultancy.co.in>
2021-07-01 14:27:02 +0530

    changes to integrate kafka

---
+++ DepartmentService/Controllers/KafkaProducerController.cs
@@ -0,0 +1,43 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DepartmentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KafkaProducerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string topic = "kafka_first_topic";
+            string message = string.Empty;
+            Uri uri = new Uri(@"http://localhost:9092");
+            var options = new KafkaOptions(uri);
+            BrokerRouter brokerRouter = new BrokerRouter(options);
+
+            Producer kafkaProducer = new Producer(brokerRouter);
+            kafkaProducer.GetTopic(topic);
+
+            //kafkaProducer.SendMessageAsync(topic, new[] { new Message() { Value = Encoding.UTF8.GetBytes("Hi I am Producer")} });
+            int count = 0;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                kafkaProducer.SendMessageAsync(topic, new[] { new Message() { Value = Encoding.UTF8.GetBytes("Hi I am Producer"+count) } });
+                count++;
+            }
+
+            return Ok(message);
+        }
+    }
+}
---
+++ EmployeeService/Controllers/KafkaConsumerController.cs
@@ -0,0 +1,37 @@
+﻿using KafkaNet;
+using KafkaNet.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KafkaConsumerController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string topic = "kafka_first_topic";
+            string message = string.Empty;
+            Uri uri = new Uri(@"http://localhost:9092");
+            var options = new KafkaOptions(uri);
+            BrokerRouter brokerRouter = new BrokerRouter(options);
+            Consumer kafkaConsumer = new Consumer(new ConsumerOptions(topic, brokerRouter));
+            var a=kafkaConsumer.GetOffsetPosition();
+            var b=kafkaConsumer.GetTopicOffsetAsync("topic");
+            foreach (var messages in kafkaConsumer.Consume())
+            {
+                message = Encoding.UTF8.GetString(messages.Value);
+                System.Diagnostics.Debug.WriteLine(message);
+                //return Ok(message);
+            }
+            return Ok(message);
+        }
+    }
+}
